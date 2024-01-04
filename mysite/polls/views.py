06bd73c8b9bd3b6fcfdea94e067bd1c8dd5a14bb
Anton Pirker <anton.pirker@sentry.io>
2024-01-04 11:57:09 +0100

    Reading s3 bucket

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,21 @@
+import boto3
+
 from django.http import HttpResponse
 
+from sentry_sdk.utils import logger
+
 def error(request):
+    # Read S3 bucket contents
+    logger.warning("Reading S3 bucket contents")
+    s3 = boto3.resource('s3')
+    my_bucket = s3.Bucket('antonpirker-test-zappa-ml4ue4qxn') # thats the zappa bucket
+    for my_bucket_object in my_bucket.objects.all():
+        logger.warning("S3 bucket object: ")
+        logger.warning(my_bucket_object)
+
+    # Raise an error
     1/0
+
     return HttpResponse("This should raise an errror!")
                         
 def index(request):
